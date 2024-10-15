bda6672d816bcbd652a70ab1d775f622e6cb4095
Michael Schneider <mschneid@mbi-berlin.de>
2024-10-15 17:38:10 +0200

    move generic getter/setter in base class

--- src/cryovacTIC500/cryovacTIC500.py
+++ src/cryovacTIC500/cryovacTIC500.py
@@ -17,101 +17,15 @@ class SensorType(IntEnum):
     ROX = 3
 
 
-class CryovacTIC500(Device):
 
-    host: str = device_property(doc="Hostname or IP address")
-    port: int = device_property(default_value=23)
+class CryovacTIC500Base:
 
-    output1_on: bool = attribute()
-    output1_value: float = attribute(
-        fget=partial(CryovacTIC500.get_channel_value, channel=OUT1),
-        fset=partial(CryovacTIC500.set_channel_value, channel=OUT1),
-        doc="Heater output power",
-        unit="W",
-    )
-    output1_setpoint: float = attribute(
-        doc="Temperature setpoint",
-        unit="K",
-        fget=partial(CryovacTIC500.get_PID_setpoint, channel=OUT1),
-        fset=partial(CryovacTIC500.set_PID_setpoint, channel=OUT1),
-    )
-    output1_ramp_setpoint: float = attribute(
-        doc="Momentary setpoint, determined by internal ramp",
-        unit="K",
-        fget=partial(CryovacTIC500.get_ramp_setpoint, channel=OUT1),
-    )
-    output1_ramp_rate: float = attribute(
-        doc="Zero disables ramping",
-        unit="K/s",
-        fget=partial(CryovacTIC500.get_ramp_rate, channel=OUT1),
-        )
-    output1_PID_on: bool = attribute(
-        doc="PID control enabled (True) or disabled (False)",
-        fget=partial(CryovacTIC500.get_PID_on, channel=OUT1),
-        fset=partial(CryovacTIC500.set_PID_on, channel=OUT1)
-    )
-    output1_P: float = attribute(
-        fget=partial(CryovacTIC500.get_PID_parameter, channel=OUT1, parameter="P"),
-        fset=partial(CryovacTIC500.set_PID_parameter, channel=OUT1, parameter="P"),
-    )
-    output1_I: float = attribute(
-        fget=partial(CryovacTIC500.get_PID_parameter, channel=OUT1, parameter="I"),
-        fset=partial(CryovacTIC500.set_PID_parameter, channel=OUT1, parameter="I"),
-    )
-    output1_D: float = attribute(
-        fget=partial(CryovacTIC500.get_PID_parameter, channel=OUT1, parameter="D"),
-        fset=partial(CryovacTIC500.set_PID_parameter, channel=OUT1, parameter="D"),
-    )
-    output1_control_channel: str = attribute(
-        doc="Input channel number to be used as PID control input",
-        fget=partial(CryovacTIC500.get_PID_input, channel=OUT1),
-        fset=partial(CryovacTIC500.set_PID_input, channel=OUT1),
-    )
-
-    input1_type: SensorType = attribute(
-        doc="Sensor type",
-        fget=partial(CryovacTIC500.get_sensor_type, channel=IN1),
-        fset=partial(CryovacTIC500.set_sensor_type, channel=IN1),
-    )
-    input1_value: float = attribute(
-        fget=partial(CryovacTIC500.get_channel_value, channel=IN1),
-        doc="Sensor input reading",
-        unit="K",
-    )
-
-    def init_device(self) -> None:
-        super().init_device()
-        try:
-            self.conn = socket.socket()
-            self.conn.connect((self.host, self.port))
-            self.conn.settimeout(0.5)
-            self.set_state(DevState.ON)
-        except Exception as exc:
-            self.set_state(DevState.FAULT)
-            self.set_status(str(exc))
-
-    @command
     def query(self, cmd: str) -> str:
-        self.conn.send(f"{cmd}\n".encode())
-        try:
-            ans = self.conn.recv(1024).decode().strip()
-        except TimeoutError:  # likely sent command with no reply
-            ans = "Timeout. Did you expect a reply?"
-        if ans.startswith("Error"):
-            raise RuntimeError(ans)
-        return ans
+        raise NotImplementedError
     
-    @command
     def send_command(self, cmd: str) -> None:
-        self.conn.send(f"{cmd}\n".encode())
-    
-    @command
-    def get_description(self) -> str:
-        return self.query("description")
-    
-    def delete_device(self) -> None:
-        self.conn.close()
-    
+        raise NotImplementedError
+
     def get_channel_value(self, channel: str) -> float:
         ans = self.query(f"{channel}.value?")
         return float(ans)
@@ -158,3 +72,100 @@ class CryovacTIC500(Device):
     
     def set_sensor_type(self, channel: str, value: SensorType) -> None:
         self.send_command(f"{channel}.Sensor={value.name}")
+
+
+class CryovacTIC500(CryovacTIC500Base, Device):
+
+    host: str = device_property(doc="Hostname or IP address")
+    port: int = device_property(default_value=23)
+
+    output1_on: bool = attribute()
+    output1_value: float = attribute(
+        fget=partial(get_channel_value, channel=OUT1),
+        fset=partial(CryovacTIC500Base.set_channel_value, channel=OUT1),
+        doc="Heater output power",
+        unit="W",
+    )
+    output1_setpoint: float = attribute(
+        doc="Temperature setpoint",
+        unit="K",
+        fget=partial(CryovacTIC500Base.get_PID_setpoint, channel=OUT1),
+        fset=partial(CryovacTIC500Base.set_PID_setpoint, channel=OUT1),
+    )
+    output1_ramp_setpoint: float = attribute(
+        doc="Momentary setpoint, determined by internal ramp",
+        unit="K",
+        fget=partial(CryovacTIC500Base.get_ramp_setpoint, channel=OUT1),
+    )
+    output1_ramp_rate: float = attribute(
+        doc="Zero disables ramping",
+        unit="K/s",
+        fget=partial(CryovacTIC500Base.get_ramp_rate, channel=OUT1),
+        )
+    output1_PID_on: bool = attribute(
+        doc="PID control enabled (True) or disabled (False)",
+        fget=partial(CryovacTIC500Base.get_PID_on, channel=OUT1),
+        fset=partial(CryovacTIC500Base.set_PID_on, channel=OUT1)
+    )
+    output1_P: float = attribute(
+        fget=partial(CryovacTIC500Base.get_PID_parameter, channel=OUT1, parameter="P"),
+        fset=partial(CryovacTIC500Base.set_PID_parameter, channel=OUT1, parameter="P"),
+    )
+    output1_I: float = attribute(
+        fget=partial(CryovacTIC500Base.get_PID_parameter, channel=OUT1, parameter="I"),
+        fset=partial(CryovacTIC500Base.set_PID_parameter, channel=OUT1, parameter="I"),
+    )
+    output1_D: float = attribute(
+        fget=partial(CryovacTIC500Base.get_PID_parameter, channel=OUT1, parameter="D"),
+        fset=partial(CryovacTIC500Base.set_PID_parameter, channel=OUT1, parameter="D"),
+    )
+    output1_control_channel: str = attribute(
+        doc="Input channel number to be used as PID control input",
+        fget=partial(CryovacTIC500Base.get_PID_input, channel=OUT1),
+        fset=partial(CryovacTIC500Base.set_PID_input, channel=OUT1),
+    )
+    input1_type: SensorType = attribute(
+        doc="Sensor type",
+        fget=partial(CryovacTIC500Base.get_sensor_type, channel=IN1),
+        fset=partial(CryovacTIC500Base.set_sensor_type, channel=IN1),
+    )
+    input1_value: float = attribute(
+        fget=partial(CryovacTIC500Base.get_channel_value, channel=IN1),
+        doc="Sensor input reading",
+        unit="K",
+    )
+
+    def init_device(self) -> None:
+        super().init_device()
+        try:
+            self.conn = socket.socket()
+            self.conn.connect((self.host, self.port))
+            self.conn.settimeout(0.5)
+            self.set_state(DevState.ON)
+        except Exception as exc:
+            self.set_state(DevState.FAULT)
+            self.set_status(str(exc))
+
+    @command
+    def query(self, cmd: str) -> str:
+        self.conn.send(f"{cmd}\n".encode())
+        try:
+            ans = self.conn.recv(1024).decode().strip()
+        except TimeoutError:  # likely sent command with no reply
+            ans = "Timeout. Did you expect a reply?"
+        if ans.startswith("Error"):
+            raise RuntimeError(ans)
+        return ans
+    
+    @command
+    def send_command(self, cmd: str) -> None:
+        self.conn.send(f"{cmd}\n".encode())
+    
+    @command
+    def get_description(self) -> str:
+        return self.query("description")
+    
+    def delete_device(self) -> None:
+        self.conn.close()
+    
+    
